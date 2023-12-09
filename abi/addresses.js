a180fa8c0f26b58a76c5869b24077e2794dec171
Yash Kamboj <yashkamboj29@gmail.cim>
2023-12-10 02:11:57 +0530

    added abi and deployed contract
    Signed-off-by: Yash Kamboj <yashkamboj29@gmail.cim>

---
+++ abi/addresses.js
@@ -0,0 +1,9 @@
+export const address = {
+    'Polygon Mumbai': '0x9814fb375038645C868B7F680C67f7B8bC60A229',
+    Alfajores: '0x4ba59fFA7985dBe24cECf45B67485Dba3Aadfb1C',
+    'Base Goerli': '0x03eF452925ecD0335BBA19E2fCf9f3E5459d0b76',
+    'Arbitrum Goerli': '0x03eF452925ecD0335BBA19E2fCf9f3E5459d0b76',
+    'Gnosis Chiado': '0x03eF452925ecD0335BBA19E2fCf9f3E5459d0b76',
+    'Neon EVM DevNet': '0x03eF452925ecD0335BBA19E2fCf9f3E5459d0b76',
+    'Scroll Sepolia': '0x03eF452925ecD0335BBA19E2fCf9f3E5459d0b76'
+}
