adffb7d9084f0d9b95b336e3f72eddf7dcd1bbc2
Yash Kamboj <yashkamboj29@gmail.cim>
2023-12-10 06:19:55 +0530

    Added custom networkand deployed on different chains
    Signed-off-by: Yash Kamboj <yashkamboj29@gmail.cim>

--- abi/addresses.js
+++ abi/addresses.js
@@ -1,7 +1,7 @@
 export const address = {
     'mumbai': '0x6eA5f433495fbc04C65aF98A1c3e68Cb20f282C5',
-    Alfajores: '0x4ba59fFA7985dBe24cECf45B67485Dba3Aadfb1C',
+    Alfajores: '0x8DD24C15c138e8948DD8858A7841E3FE8bF87cA5',
     'Base Goerli': '0x2e137b830782826328D94f0AC5848Ce08f25f547',
-    'Arbitrum Goerli': '0x2e137b830782826328d94f0ac5848ce08f25f547',
+    'Arbitrum Sepolia Testnet': '0x2e137b830782826328d94f0ac5848ce08f25f547',
     'Scroll Sepolia': '0x9814fb375038645C868B7F680C67f7B8bC60A229'
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,60 +1,127 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import {
   polygonMumbai,
   celoAlfajores,
   arbitrumGoerli,
   baseGoerli,
-  gnosisChiado,
-  neonDevnet,
-  scrollSepolia
-} from 'wagmi/chains';
+  scrollSepolia,
+  
+} from "wagmi/chains";
 
-import { publicProvider } from 'wagmi/providers/public';
-import { ThemeProvider } from 'styled-components';
-import { ThorinGlobalStyles, darkTheme } from '@ensdomains/thorin';
-import { ChakraProvider, Box, Heading, Flex, Text } from '@chakra-ui/react';
+import { publicProvider } from "wagmi/providers/public";
+import { ThemeProvider } from "styled-components";
+import { ThorinGlobalStyles, darkTheme } from "@ensdomains/thorin";
+import { ChakraProvider, Box, Heading, Flex, Text } from "@chakra-ui/react";
 
-import { Chain } from 'wagmi'
+import { Chain } from "wagmi";
 
 export const mumbai = {
   id: 80001,
-  name: 'mumbai',
-  network: 'mumbai',
+  name: "mumbai",
+  network: "mumbai",
   nativeCurrency: {
     decimals: 18,
-    name: 'matic',
-    symbol: 'matic',
+    name: "matic",
+    symbol: "matic",
   },
   rpcUrls: {
-    public: { http: ['https://polygon-testnet.public.blastapi.io'] },
-    default: { http: ['https://polygon-testnet.public.blastapi.io'] },
+    public: { http: ["https://polygon-testnet.public.blastapi.io"] },
+    default: { http: ["https://polygon-testnet.public.blastapi.io"] },
   },
   blockExplorers: {
-    etherscan: { name: 'SnowTrace', url: 'https://snowtrace.io' },
-    default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    etherscan: { name: "SnowTrace", url: "https://snowtrace.io" },
+    default: { name: "SnowTrace", url: "https://snowtrace.io" },
   },
-} 
+};
 
+export const scroll = {
+  id: 534351,
+  name: "Scroll Sepolia",
+  network: "Scroll Sepolia",
+  nativeCurrency: {
+    decimals: 18,
+    name: "eth",
+    symbol: "eth",
+  },
+  rpcUrls: {
+    public: { http: ["https://sepolia-rpc.scroll.io"] },
+    default: { http: ["https://sepolia-rpc.scroll.io"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "SnowTrace", url: "https://snowtrace.io" },
+    default: { name: "SnowTrace", url: "https://snowtrace.io" },
+  },
+};
+
+export const arbitrum = {
+  id: 421614,
+  name: "Arbitrum Sepolia Testnet",
+  network: "Arbitrum Sepolia Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "eth",
+    symbol: "eth",
+  },
+  rpcUrls: {
+    public: { http: ["https://sepolia-rollup.arbitrum.io/rpc"] },
+    default: { http: ["https://sepolia-rollup.arbitrum.io/rpc"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "SnowTrace", url: "https://sepolia.arbiscan.io" },
+    default: { name: "SnowTrace", url: "https://sepolia.arbiscan.io" },
+  },
+};
+
+export const base = {
+  id: 84531,
+  name: "Base Goerli",
+  network: "Base Goerl",
+  nativeCurrency: {
+    decimals: 18,
+    name: "eth",
+    symbol: "eth",
+  },
+  rpcUrls: {
+    public: { http: ["https://goerli.base.org"] },
+    default: { http: ["https://goerli.base.org"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "SnowTrace", url: " https://goerli.basescan.org" },
+    default: { name: "SnowTrace", url: " https://goerli.basescan.org" },
+  },
+};
+
+
+export const mantle = {
+  id: 5001,
+  name: "Mantle testnet",
+  network: "Mantle testne",
+  nativeCurrency: {
+    decimals: 18,
+    name: "mnt",
+    symbol: "mnt",
+  },
+  rpcUrls: {
+    public: { http: ["https://rpc.testnet.mantle.xyz"] },
+    default: { http: ["https://rpc.testnet.mantle.xyz"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "SnowTrace", url: "https://explorer.mantle.xyz/" },
+    default: { name: "SnowTrace", url: "https://explorer.mantle.xyz/" },
+  },
+};
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-mumbai,
-    celoAlfajores,
-    arbitrumGoerli,
-    baseGoerli,
-    gnosisChiado,
-    neonDevnet,
-    scrollSepolia
-  ],
+  [mumbai, celoAlfajores, arbitrum, base, scrollSepolia],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'PeerPurse',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: "PeerPurse",
+  projectId: "YOUR_PROJECT_ID",
   chains,
 });
 
@@ -72,11 +139,23 @@ function MyApp({ Component, pageProps }) {
         <ThorinGlobalStyles />
         <WagmiConfig config={wagmiConfig}>
           <RainbowKitProvider chains={chains}>
-            <Box minH="100vh"  bg="#282c34" color="white" display="flex" flexDirection="column">
-              <Flex as="main" flex="1"  align="center" direction="column">
+            <Box
+              minH="100vh"
+              bg="#282c34"
+              color="white"
+              display="flex"
+              flexDirection="column"
+            >
+              <Flex as="main" flex="1" align="center" direction="column">
                 <Component {...pageProps} />
               </Flex>
-              <Box as="footer" p="1rem" borderTop="1px solid #444c5d" bg="#1e232b" textAlign="center">
+              <Box
+                as="footer"
+                p="1rem"
+                borderTop="1px solid #444c5d"
+                bg="#1e232b"
+                textAlign="center"
+              >
                 <Text fontSize="0.9rem" color="#888">
                   &copy; 2023 PeerPurse
                 </Text>
